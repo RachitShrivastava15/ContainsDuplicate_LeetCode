d1e2ee6bb39eaa542fb521d09c93ac44bf9f46db
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-01 17:15:38 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ContainsDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,2,3,1};
+            bool result = ContainsDuplicate(nums);
+            Console.WriteLine(result);
+        }
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hashValues = new HashSet<int>();
+
+            for(int index = 0; index < nums.Length; index ++)
+            {
+                if (!hashValues.Add(nums[index]))
+                return true;
+            }
+            return false;
+        }
+    }
+}
